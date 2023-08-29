3e97c3d9324c6a64a33933b54e0366660f9e9da1
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-08-29 18:05:43 +0300

    cc

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose')
 
-const DB_HOST = "mongodb+srv://Mykhailo:7C8pmJPNnf8P7GW6@cluster0.jyy739w.mongodb.net/db-contacts?retryWrites=true&w=majority";
+// const DB_HOST = "mongodb+srv://Mykhailo:7C8pmJPNnf8P7GW6@cluster0.jyy739w.mongodb.net/db-contacts?retryWrites=true&w=majority";
 
-mongoose.connect(DB_HOST)
-  .then(() => {
-    app.listen(3000)
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
-  })
+// mongoose.connect(DB_HOST)
+//   .then(() => {
+//     app.listen(3000)
+//   })
+//   .catch(error => {
+//     console.log(error.message);
+//     process.exit(1);
+//   })
 
-// const app = require('./app')
+const app = require('./app')
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
+})
